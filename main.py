a0e2dee01d8f04d9f7777abe4c18c09a43b220aa
Gaurav Singh <grv08singh@gmail.com>
2025-10-04 19:34:57 +0530

    creating env

---
+++ main.py
@@ -0,0 +1,23 @@
+# Modules
+
+
+# Class
+    # Constructor
+
+    # App Object and Design
+
+    # App Settings
+
+    # button events
+
+    # Translate Click
+
+    # Reset App
+
+    # Translate Text (Google)
+
+    # Reverse Translation
+
+
+
+# Main Run
